8dcb3a50533ea1405b9b7b24734d6ca26940016e
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-22 22:32:08 +0500

    Simple calc

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hi")
+let num1 = prompt("num1")
+let num2 = prompt("num2")
+let operator = prompt("operator (+, -, *, /)")
+
+function operate(a, b, c){
+    let answ;
+    if (operator === "+"){
+        answ = a + b;
+    }  else if (operator === "-"){
+        answ = a - b;
+    }  else if (operator === "*"){
+        answ = a * b;
+    }  else if (operator === "/"){
+        answ = a / b;
+    }
+    
+    console.log(answ);
+}
+
+operate(parseInt(num1), parseInt(num2))
