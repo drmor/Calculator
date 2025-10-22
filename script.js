cc12cc21b4f064f078ebdee6a4aa257d8e8b6456
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-22 23:33:06 +0500

    did css so now we can see calc on web page, not working

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let num1 = prompt("num1")
-let num2 = prompt("num2")
-let operator = prompt("operator (+, -, *, /)")
+// const container = document.querySelector(".container");
+// const btnOperator = document.querySelector("button");
 
-function operate(a, b, c){
-    let answ;
-    if (operator === "+"){
-        answ = a + b;
-    }  else if (operator === "-"){
-        answ = a - b;
-    }  else if (operator === "*"){
-        answ = a * b;
-    }  else if (operator === "/"){
-        answ = a / b;
-    }
+
+
+// let num1 = prompt("num1")
+// let num2 = prompt("num2")
+// let operator = prompt("operator (+, -, *, /)")
+
+// function operate(a, b, c){
+//     let answ;
+//     if (operator === "+"){
+//         answ = a + b;
+//     }  else if (operator === "-"){
+//         answ = a - b;
+//     }  else if (operator === "*"){
+//         answ = a * b;
+//     }  else if (operator === "/"){
+//         answ = a / b;
+//     }
     
-    console.log(answ);
-}
+//     console.log(answ);
+// }
 
-operate(parseInt(num1), parseInt(num2))
+// operate(parseInt(num1), parseInt(num2))
