b5f33b75a552007bb7486e470dae9ac47218f089
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-23 23:34:59 +0500

    Did better CSS, now you can store values of first and second number and an operator

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
-// const container = document.querySelector(".container");
-// const btnOperator = document.querySelector("button");
+const btnNumbers = document.querySelector(".numbers");
+const btnOperators = document.querySelector(".operators");
+const btnEqual = document.querySelector(".equal");
+const output = document.querySelector(".output");
+output.textContent = "0";
 
+let firstNum;
+let secondNum;
+let operatorChoice;
+let changeNum = false;
+getValues()
 
+function getValues(){
+    let arrNum1 = [];
+    let arrNum2 = [];
+    btnOperators.addEventListener("click", (e) =>{
+        if (!firstNum){
+            changeNum = false;
+        } else {
+            changeNum = true;
+        }
+        operatorChoice = e.target.id;
+        output.textContent = operatorChoice;
+    }); 
+    btnNumbers.addEventListener("click", (e) =>{
+        const elementId = e.target.id;
+        if (!changeNum){
+            arrNum1.push(elementId);
+            firstNum = parseInt(arrNum1.join(""));
+            output.textContent = firstNum;
+        } else {
+            arrNum2.push(elementId);
+            secondNum = parseInt(arrNum2.join("")); 
+            output.textContent = secondNum;
+        };
+    });
+};
+let answ;
+add()
+function populate(){
 
-// let num1 = prompt("num1")
-// let num2 = prompt("num2")
-// let operator = prompt("operator (+, -, *, /)")
+};
 
-// function operate(a, b, c){
-//     let answ;
-//     if (operator === "+"){
-//         answ = a + b;
-//     }  else if (operator === "-"){
-//         answ = a - b;
-//     }  else if (operator === "*"){
-//         answ = a * b;
-//     }  else if (operator === "/"){
-//         answ = a / b;
-//     }
-    
-//     console.log(answ);
-// }
+function operate(){
 
-// operate(parseInt(num1), parseInt(num2))
+};
+function add(){
+    output.textContent = " ";
+    btnEqual.addEventListener("click", () =>{
+        answ = firstNum + secondNum;
+        
+    });
+    output.textContent = answ;
+};
+function subtract(){
+
+};
+function multiply(){
+
+};
+function divide(){
+
+};
