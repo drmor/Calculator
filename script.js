750ab05af3158f52e2dedc09e397effe7204265f
Miki D <69644623+drmor@users.noreply.github.com.>
2025-10-25 20:03:07 +0500

    Now  you can actually calculate but without populate

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const btnNumbers = document.querySelector(".numbers");
 const btnOperators = document.querySelector(".operators");
 const btnEqual = document.querySelector(".equal");
-const output = document.querySelector(".output");
-output.textContent = "0";
+const outputBar = document.querySelector(".output");
 
-let firstNum;
-let secondNum;
+
+let firstNum = 0;
+let answer = 0;
+let secondNum = 0;
 let operatorChoice;
 let changeNum = false;
 getValues()
+operate()
 
 function getValues(){
-    let arrNum1 = [];
-    let arrNum2 = [];
     btnOperators.addEventListener("click", (e) =>{
         if (!firstNum){
             changeNum = false;
@@ -20,25 +20,15 @@ function getValues(){
             changeNum = true;
         }
         operatorChoice = e.target.id;
-        output.textContent = operatorChoice;
     }); 
     btnNumbers.addEventListener("click", (e) =>{
         const elementId = e.target.id;
         if (!changeNum){
-            arrNum1.push(elementId);
-            firstNum = parseInt(arrNum1.join(""));
-            output.textContent = firstNum;
+            firstNum += elementId
         } else {
-            arrNum2.push(elementId);
-            secondNum = parseInt(arrNum2.join("")); 
-            output.textContent = secondNum;
+            secondNum += elementId || 0
         };
     });
-};
-let answ;
-add()
-function populate(){
-
 };
 
 function operate(){
@@ -52,18 +42,27 @@ function operate(){
         } else if (operatorChoice === "/"){
             divide();
         };
+        console.log("before", firstNum, secondNum);
+        firstNum = answer;
+        secondNum = null;
+        console.log("after", firstNum, secondNum);
+        outputBar.textContent = answer;
     });
 };
 
 function add(){
-    return firstNum + secondNum;
+    answer = parseInt(firstNum) + parseInt(secondNum);
+    return answer;
 };
 function subtract(){
-    return firstNum - secondNum;
+    answer = parseInt(firstNum) - parseInt(secondNum);
+    return answer;
 };
 function multiply(){
-    return firstNum * secondNum;
+    answer = parseInt(firstNum) * parseInt(secondNum);
+    return answer;
 };
 function divide(){
-    return firstNum / secondNum;
+    answer = parseInt(firstNum) / parseInt(secondNum);
+    return answer;
 };
